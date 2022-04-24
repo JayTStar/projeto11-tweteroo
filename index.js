d08d2003418c4630d31696f8c6d723db66c012e8
Jorge Tadeu <jsbatalha@gmail.com>
2022-04-24 19:11:03 -0300

    Projeto Sem bonus

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+let usuarios = [];
+let tweets =[];
+
+app.post("/sign-up", (req, res) => {
+    const usuario = req.body;
+    usuarios.push(usuario);
+    res.send("OK");
+});
+
+app.post("/tweets", (req,res) => {
+    const tweet = req.body;
+    tweets.unshift(tweet);
+    res.send("OK");
+})
+
+app.get("/tweets", (req, res) => {
+    res.send(tweets.slice(0, 10));
+})
+
+app.listen(5000)
