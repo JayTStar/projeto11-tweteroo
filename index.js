c60633460fdb18139b911fc548d670df921e759c
Jorge Tadeu <jsbatalha@gmail.com>
2022-04-25 10:22:47 -0300

    Bonus!

--- index.js
+++ index.js
@@ -8,20 +8,52 @@ app.use(cors());
 let usuarios = [];
 let tweets =[];
 
+function verificaVazio(dado){
+    if(dado === undefined || dado === "" || dado === null){
+        return false
+    }
+    else{
+        return true
+    }
+}
+
 app.post("/sign-up", (req, res) => {
     const usuario = req.body;
-    usuarios.push(usuario);
-    res.status(200).send("OK");
+    
+    if(verificaVazio(usuario.username) === false || verificaVazio(usuario.avatar) === false){
+        res.status(400).send("Todos os campos são obrigatórios!");
+    }
+    else{
+        usuarios.push(usuario);
+        res.status(201).send("Created");
+    }
 });
 
 app.post("/tweets", (req,res) => {
-    const tweet = req.body;
-    tweets.unshift(tweet);
-    res.status(200).send("OK");
+    const tweet = {
+        username: req.headers.user,
+        tweet: req.body.tweet
+    }
+
+    if(verificaVazio(tweet.username) === false || verificaVazio(tweet.tweet) === false){
+        res.status(400).send("Todos os campos são obrigatórios!");
+    }
+    else{
+        tweets.unshift(tweet);
+        res.status(201).send("Created");
+    }
 })
 
 app.get("/tweets", (req, res) => {
     res.send(tweets.slice(0, 10));
 })
 
-app.listen(5000)
+app.get('/tweets/:username', (req, res) => {
+    const usuario = req.params.username;
+
+    const tweetsusuario = tweets.filter(elemento => elemento.username === usuario);
+
+    res.send(tweetsusuario);
+})
+
+app.listen(5000);
